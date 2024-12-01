bc3ec822d2ebb0c66d4f8ff07adc5242bc6a9619
niraj <nirajshinde619@gmail.com>
2024-12-01 17:17:08 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys ##any exception that is basically getting controlled, the sys library will automatically have that information
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,4 +18,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
 
