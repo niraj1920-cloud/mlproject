56fe5adacea28499d636072662c7c096ec9cc7e6
niraj <nirajshinde619@gmail.com>
2024-12-06 08:43:02 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,21 +21,33 @@ def save_object(file_path,obj): ##for saving the pickle file
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train,y_train) ##Train model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(x_train)
+
             y_test_pred = model.predict(x_test)
-            train_model_score = r2_score(y_train , y_train_pred)
-            test_model_score = r2_score(y_test , y_test_pred)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
             report[list(models.keys())[i]] = test_model_score
 
         return report
 
-    except:
-        pass
-    
+    except Exception as e:
+        raise CustomException(e, sys)
